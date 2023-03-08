8e7872dd13b2578f76fbe2621a7aaa89554aeebf
Emad <emabbasi@cougarnet.uh.edu>
2023-03-08 10:23:10 -0600

    Fixing Errors in Input

--- Homework 2/Zylab_6.17.py
+++ Homework 2/Zylab_6.17.py
@@ -1,1 +1,15 @@
+# 2095022 #EMADABBASI
+# PASSWORD MODIFIER
+# first replacing characters with specials
+password = input()
+password = password.replace('i', '!')
+password = password.replace('a', '@')
+password = password.replace('m', 'M')
+password = password.replace('B', '8')
+password = password.replace('o', '.')
+# appending
+password += 'q*s'
+password = password.capitalize()
+
+print(password)
 
