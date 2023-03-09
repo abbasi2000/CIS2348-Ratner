e82ecf0c36382d044ad19ee003b4818f6bbca763
Emad <emabbasi@cougarnet.uh.edu>
2023-03-08 18:58:21 -0600

    Fixing Errors in Input

--- Homework 2/Homework#2.py
+++ Homework 2/Homework#2.py
@@ -2,3 +2,16 @@
 # DATE READER/MODIFIER
 # TRYING TO UNDERSTAND COMMIT CHANGES, i'll write the full code here then figure that out later
 # initial commit change done, visible in history but still unsure on how to add a change
+from datetime import datetime
+
+with open('inputDates.txt', 'r') as f:
+    for line in f:
+        date_str = line.strip()
+        if date_str == "-1":
+            break
+        try:
+            date = datetime.strptime(date_str, '%B %d, %Y')
+            if date <= datetime.now():
+                print(date.strftime('%-m/%-d/%Y'))
+        except ValueError:
+            pass
