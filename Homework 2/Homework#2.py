cc18cafa6331bc7fe236a8b9c6b048fcc788cabe
Emad <emabbasi@cougarnet.uh.edu>
2023-03-08 22:53:48 -0600

    Fixing Errors in Input

--- Homework 2/Homework#2.py
+++ Homework 2/Homework#2.py
@@ -4,11 +4,23 @@
 # initial commit change done, visible in history but still unsure on how to add a change
 from datetime import datetime
 
-with open('inputDates.txt', 'r') as f:
-    for line in f:
-        date_str = line.strip()
-        if date_str == "-1":
-            break
+# Part A Write a program that reads dates from input, one date per line. Each
+# date's format must be as follows: March 1, 1990. Ignore any dates
+# that are later than the current date. Output each correct date as:
+# 3/1/1990.
+
+while True:
+    date_string = input()
+    if date_string == '-1':
+        break
+    try:
+        date_obj = datetime.datetime.strptime(date_string, '%B %d, %Y')
+        current_date = datetime.datetime.now()
+        if date_obj <= current_date:
+            print(date_obj.strftime('%-m/%-d/%Y'))
+    except ValueError:
+        pass
+
         try:
             date = datetime.strptime(date_str, '%B %d, %Y')
             if date <= datetime.now():
