017bc9b49332a6bd5c94dc56a77d9b3e9ff370f4
Emad <emabbasi@cougarnet.uh.edu>
2023-05-09 20:35:41 -0500

    Fixing Errors in Input

--- FinalProjectPart1/FinalProjectInventory.py
+++ FinalProjectPart1/FinalProjectInventory.py
@@ -85,7 +85,7 @@ while True:
     include_damaged = False
     damaged_input = ""
     try:
-        manufacturer, item_type = input("Enter the manufacturer & item type with ; as the delimeter. Enter 'q' or 'exit' to quit: ").split(";")
+        manufacturer, item_type = input("Enter the manufacturer & item type with , as the delimeter. Enter 'q' or 'exit' to quit: ").split(",")
         damaged_input = input("Should damaged items be included (Y/N): ")
     except ValueError:
         if manufacturer.lower() == 'q' or manufacturer.lower() == 'exit':
